308165e6fc6a2769911faea93f3411bea48cfcc8
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2023-08-20 11:14:14 +0530

    Improve system into preventing purchase an invalid seat

--- src/main/java/com/example/mysql_studentms_youtube_intellij/BookController.java
+++ src/main/java/com/example/mysql_studentms_youtube_intellij/BookController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -33,7 +34,6 @@ public class BookController extends TicketController {
     public void bookTicket() {
         getTextFile();
         getSeatInfo();
-        save();
     }
 
     //Load the text file that contains reserved seat info
@@ -85,33 +85,59 @@ public class BookController extends TicketController {
         secondName = data.getSecondName();
         email = data.getEmail();
 
+        int seat = Integer.parseInt(myTextFiled.getText());
 
         if (radio1.isSelected()) {
-            String row = radio1.getText();
-            int seat = Integer.parseInt(myTextFiled.getText());
-            row1[seat-1] = 1;
-            infoText.setText("You Successfully Reserved " + row + " : SEAT " + seat);
 
-            myTicket = new Ticket(firstName.toUpperCase(), secondName.toUpperCase(), email.toLowerCase(), 1, seat);
+            insertData(firstName, secondName, email, 1, seat, radio1, 1, 12, row1);
+//            String row = radio1.getText();
+//            int seat = Integer.parseInt(myTextFiled.getText());
+//            row1[seat-1] = 1;
+//            infoText.setText("You Successfully Reserved " + row + " : SEAT " + seat);
+//
+//            myTicket = new Ticket(firstName.toUpperCase(), secondName.toUpperCase(), email.toLowerCase(), 1, seat);
         }
         else if (radio2.isSelected()) {
-            String row = radio2.getText();
-            int seat = Integer.parseInt(myTextFiled.getText());
-            row2[seat-1] = 1;
-            infoText.setText("You Successfully Reserved " + row + " : SEAT " + seat);
 
-            myTicket = new Ticket(firstName.toUpperCase(), secondName.toUpperCase(), email.toLowerCase(), 2, seat);
+            insertData(firstName, secondName, email, 2, seat, radio2, 1, 16, row2);
+//            String row = radio2.getText();
+//            row2[seat-1] = 1;
+//            infoText.setText("You Successfully Reserved " + row + " : SEAT " + seat);
+//
+//            myTicket = new Ticket(firstName.toUpperCase(), secondName.toUpperCase(), email.toLowerCase(), 2, seat);
         }
         else if (radio3.isSelected()) {
-            String row = radio3.getText();
-            int seat = Integer.parseInt(myTextFiled.getText());
-            row3[seat-1] = 1;
-            infoText.setText("You Successfully Reserved " + row + " : SEAT " + seat);
 
-            myTicket = new Ticket(firstName.toUpperCase(), secondName.toUpperCase(), email.toLowerCase(), 3, seat);
+            insertData(firstName, secondName, email, 3, seat, radio3, 1, 20, row3);
+//            String row = radio3.getText();
+//            row3[seat-1] = 1;
+//            infoText.setText("You Successfully Reserved " + row + " : SEAT " + seat);
+//
+//            myTicket = new Ticket(firstName.toUpperCase(), secondName.toUpperCase(), email.toLowerCase(), 3, seat);
         }
 
-        dataArray.addData(myTicket);
+    }
+
+    protected void insertData(String firstName, String secondName, String email, int row_num, int seat_num,
+                              RadioButton radioBtn, int firstSeatNum, int lastSeatNum,
+                              int[] rowArray){
+
+        String row = radioBtn.getText();
+
+        if (seat_num >= firstSeatNum && seat_num <= lastSeatNum && rowArray[seat_num-1] == 0){
+            rowArray[seat_num-1] = 1;
+            infoText.setTextFill(Color.rgb(10, 168, 52));
+            infoText.setText("You Successfully Reserved " + row + " : SEAT " + seat_num);
+
+            myTicket = new Ticket(firstName.toUpperCase(), secondName.toUpperCase(), email.toLowerCase(), row_num, seat_num);
+
+            dataArray.addData(myTicket);
+            save();
+        }
+        else {
+            infoText.setTextFill(Color.RED);
+            infoText.setText("Seat selection can not be proceed! Check the seating area and try again!");
+        }
 
     }
 
